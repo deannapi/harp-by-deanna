3bfdc304bfc0dc42d7de2b7246c571796f68a7ae
deannapi <deannapi.mart@gmail.com>
2021-05-06 10:32:14 -0500

    updated linkedin

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -12,7 +12,7 @@ export default class Footer extends React.Component {
             <ul>
               <li>
                 <a
-                  href="https://www.linkedin.com/in/deanna-martinez-63539a137/"
+                  href="https://www.linkedin.com/in/deanna-barker-63539a137/"
                   target="_blank"
                   rel="noreferrer"
                 >
